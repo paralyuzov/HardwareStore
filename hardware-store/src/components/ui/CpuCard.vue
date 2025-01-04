640f1cddeaf5c1a6c45fc2a744522d437dca24e4
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-01-04 20:37:18 +0200

    add cpu card component

---
+++ hardware-store/src/components/ui/CpuCard.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+  name: "CpuCard",
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+  methods: {
+    navigateToDetails() {
+      this.$router.push(`/catalog/cpu/${this.id}`);
+    }
+
+  }
+}
+</script>
+
+<template>
+  <div
+    class="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300 cursor-pointer flex flex-col justify-center items-center hover:scale-105"
+    @click="navigateToDetails"
+  >
+    <div>
+      <img
+        v-if="image"
+        :src="image"
+        :alt="name"
+        class="w-52 object-fit"
+      />
+    </div>
+    <div class="p-4 flex flex-col items-center">
+      <h2 class="text-lg font-semibold text-gray-800 truncate">{{ name }}</h2>
+      <p class="text-lg font-bold text-gray-800 mt-2">${{ price.toFixed(2) }}</p>
+    </div>
+  </div>
+</template>
+

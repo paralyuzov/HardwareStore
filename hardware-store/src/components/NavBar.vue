251b6504420e8a865138854aa9dd7b8b45a314c9
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-01-05 22:00:01 +0200

    add router-link for video cards

--- hardware-store/src/components/NavBar.vue
+++ hardware-store/src/components/NavBar.vue
@@ -56,7 +56,9 @@ export default {
             <router-link to="/catalog/ssd" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">
               SSD
             </router-link>
-            <a href="#" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">Video Cards</a>
+            <router-link to="/catalog/videocard" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+              Video Cards
+            </router-link>
             <a href="#" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">PSU</a>
           </div>
         </div>

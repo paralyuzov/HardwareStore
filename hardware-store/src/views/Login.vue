6776ba7d8fbd5f5590197edd1211dc2d6f994a64
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-01-08 19:48:01 +0200

    Add Register Link to Login Page

--- hardware-store/src/views/Login.vue
+++ hardware-store/src/views/Login.vue
@@ -13,6 +13,16 @@ export default {
   <div class="flex justify-center items-center min-h-screen bg-gray-100">
     <div class="w-full max-w-lg bg-gray-100 p-6 rounded-lg shadow-lg z-50">
       <LoginForm />
+      <div class="mt-4 text-center">
+      <p class="text-gray-700">
+        Don't have an account?
+        <router-link to="/register" class="text-blue-500 hover:underline">
+          Register here
+        </router-link>
+      </p>
     </div>
+    </div>
+
   </div>
+
 </template>

1df6b62218d820d3f0ae92063f8996da01d868c7
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-01-09 19:18:14 +0200

    fix styles

--- hardware-store/src/components/NavBar.vue
+++ hardware-store/src/components/NavBar.vue
@@ -40,66 +40,66 @@ export default {
       <div class="flex items-center space-x-10 font-serif">
 
         <div class="relative group">
-          <p class="text-gray-700 hover:text-gray-900 focus:outline-none cursor-pointer">
+          <p class="text-white focus:outline-none cursor-pointer bg-blue-500 px-4 py-2 rounded-xl">
             PRODUCTS
           </p>
           <div class="absolute hidden group-hover:block -right-20 pt-1 w-48 border bg-white rounded-md shadow-lg z-50">
-            <router-link to="/catalog/laptop" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+            <router-link to="/catalog/laptop" class="block px-4 py-2 text-black rounded-2xl hover:bg-blue-500 hover:text-white">
               Laptops
             </router-link>
-            <router-link to="/catalog/cpu" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+            <router-link to="/catalog/cpu" class="block px-4 py-2 text-black rounded-2xl hover:bg-blue-500 hover:text-white">
               CPU
             </router-link>
-            <router-link to="/catalog/motherboard" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+            <router-link to="/catalog/motherboard" class="block px-4 py-2 text-black rounded-2xl hover:bg-blue-500 hover:text-white">
               Motherboards
             </router-link>
-            <router-link to="/catalog/ram" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+            <router-link to="/catalog/ram" class="block px-4 py-2 text-black rounded-2xl hover:bg-blue-500 hover:text-white">
               RAM
             </router-link>
-            <router-link to="/catalog/ssd" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+            <router-link to="/catalog/ssd" class="block px-4 py-2 text-black rounded-2xl hover:bg-blue-500 hover:text-white">
               SSD
             </router-link>
-            <router-link to="/catalog/videocard" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+            <router-link to="/catalog/videocard" class="block px-4 py-2 text-black rounded-2xl hover:bg-blue-500 hover:text-white">
               Video Cards
             </router-link>
-            <router-link to="/catalog/psu" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+            <router-link to="/catalog/psu" class="block px-4 py-2 text-black rounded-2xl hover:bg-blue-500 hover:text-white">
               PSU
             </router-link>
           </div>
         </div>
 
         <div class="relative group">
-          <div v-if="isLoggedIn" class="flex items-center space-x-2 cursor-pointer focus:outline-none">
+          <div v-if="isLoggedIn" class="flex items-center space-x-2 cursor-pointer focus:outline-none bg-blue-500 px-4 py-2 rounded-xl text-white">
             <i class="fa-solid fa-user"></i>
             <span>{{ user?.username || 'User' }}</span>
           </div>
-          <div v-else class="flex items-center space-x-2 cursor-pointer focus:outline-none">
+          <div v-else class="flex items-center space-x-2 cursor-pointer focus:outline-none  bg-blue-500 px-4 py-2 rounded-xl text-white">
             <i class="fa-solid fa-user"></i>
             <span>Guest</span>
           </div>
           <div class="absolute hidden group-hover:block -right-20 pt-1 border w-48 bg-white rounded-md shadow-lg z-50">
-            <router-link v-if="isAdmin" to="/admin" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+            <router-link v-if="isAdmin" to="/admin" class="block px-4 py-2 text-black rounded-2xl hover:bg-blue-500 hover:text-white">
               Admin Panel
             </router-link>
-            <router-link v-if="isLoggedIn" to="/orders" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+            <router-link v-if="isLoggedIn" to="/orders" class="block px-4 py-2 text-black rounded-2xl hover:bg-blue-500 hover:text-white">
               Orders
             </router-link>
-            <router-link v-if="isLoggedIn" to="/profile" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+            <router-link v-if="isLoggedIn" to="/profile" class="block px-4 py-2 text-black rounded-2xl hover:bg-blue-500 hover:text-white">
               Profile
             </router-link>
-            <a v-else href="/login" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+            <a v-else href="/login" class="block px-4 py-2 text-black hover:bg-blue-500 rounded-2xl hover:text-white">
               Login
             </a>
             <button v-if="isLoggedIn" @click="handleLogout"
-              class="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100">
+              class="block w-full text-left px-4 py-2 text-black hover:bg-blue-500 rounded-2xl hover:text-white">
               Logout
             </button>
           </div>
         </div>
-        <div class="relative group bg-sky-100 p-2 rounded-full">
+        <div class="relative group bg-sky-100 p-4 rounded-full">
           <router-link to="/cart" class="flex items-center space-x-2 cursor-pointer">
             <i class="fa-solid fa-cart-shopping text-gray-700 hover:scale-105"></i>
-            <div v-if="cartItemCount > 0" class="absolute top-5 left-5 bg-red-500 text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
+            <div v-if="cartItemCount > 0" class="absolute top-7 left-7 bg-red-500 text-white text-xs w-6 h-6 rounded-full flex items-center justify-center">
               {{ cartItemCount }}
             </div>
           </router-link>
